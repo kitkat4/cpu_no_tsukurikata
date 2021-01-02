8533e2cbe4466134d85d4534ece4d4d28b05233d
Katsumasa Kitajima <k.k4.bky@gmail.com>
2021-01-02 23:05:40 +0900

    Fixed clock_gen

--- cpu15/verilog/clock_gen.v
+++ cpu15/verilog/clock_gen.v
@@ -20,19 +20,27 @@ module clock_gen(CLK, CLK_FT, CLK_DC, CLK_EX, CLK_WB);
         
         if(COUNT == 2'b00) begin
             CLK_FT <= 1;
+            CLK_DC <= 0;
             CLK_EX <= 0;
+            CLK_WB <= 0;
         end 
         else if(COUNT == 2'b01) begin
+            CLK_FT <= 0;
             CLK_DC <= 1;
+            CLK_EX <= 0;
             CLK_WB <= 0;
         end 
         else if(COUNT == 2'b10) begin
-            CLK_EX <= 1;
             CLK_FT <= 0;
+            CLK_DC <= 0;
+            CLK_EX <= 1;
+            CLK_WB <= 0;
         end 
         else if(COUNT == 2'b11) begin
-            CLK_WB <= 1;
+            CLK_FT <= 0;
             CLK_DC <= 0;
+            CLK_EX <= 0;
+            CLK_WB <= 1;
         end
         
     end
